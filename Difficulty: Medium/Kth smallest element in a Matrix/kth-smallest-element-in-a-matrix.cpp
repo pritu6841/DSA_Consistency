27a965038829dbdf6a4669c926b8c5605871068e
pritu6841 <pritu6841@gmail.com>
2025-08-04 17:17:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth smallest element in a Matrix/kth-smallest-element-in-a-matrix.cpp
+++ Difficulty: Medium/Kth smallest element in a Matrix/kth-smallest-element-in-a-matrix.cpp
@@ -2,16 +2,27 @@ class Solution {
   public:
     int kthSmallest(vector<vector<int>> &mat, int k) {
         // code here
-        priority_queue<int> pq;
+        vector<pair<int, pair<int, int>>>temp;
+        int n = mat.size();
+        for(int i = 0; i<n; i++) temp.push_back(make_pair(mat[i][0], make_pair(i, 0)));
         
-        for (int i = 0; i < mat.size(); i++) {
-            for (int j = 0; j < mat[0].size(); j++) {
-                pq.push(mat[i][j]);
-                if (pq.size() > k) {
-                    pq.pop();  // remove the largest
-                }
-            }
+        
+        priority_queue<pair<int, pair<int,int>>, 
+               vector<pair<int, pair<int,int>>>, 
+               greater<pair<int, pair<int,int>>>> pq(temp.begin(), temp.end());
+
+        int ans;
+        int i, j;
+        
+        while(k--){
+            pair<int, pair<int, int>> pr = pq.top();
+            ans = pr.first;
+            i = pr.second.first;
+            j = pr.second.second;
+            pq.pop();
+            if(j+1 < mat[i].size())
+            pq.push(make_pair(mat[i][j+1], make_pair(i, j+1)));
         }
-        return pq.top();  
+        return ans;
     }
 };
