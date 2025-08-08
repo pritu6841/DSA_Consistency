08b0d492150a6f1ad4b28ed040592dea463bdcbc
pritu6841 <pritu6841@gmail.com>
2025-08-08 16:33:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Longest Prefix Suffix/longest-prefix-suffix.py
@@ -0,0 +1,21 @@
+class Solution:
+    def getLPSLength(self, s: str) -> int:
+        n = len(s)
+        lps = [0] * n
+        length = 0  # length of the previous longest prefix suffix
+        i = 1
+
+        while i < n:
+            if s[i] == s[length]:
+                length += 1
+                lps[i] = length
+                i += 1
+            else:
+                if length != 0:
+                    length = lps[length - 1]
+                else:
+                    lps[i] = 0
+                    i += 1
+        
+        return lps[-1]
+
