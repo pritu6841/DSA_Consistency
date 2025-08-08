a15fba7413897ae897b75847bb0c43328e79ea79
pritu6841 <pritu6841@gmail.com>
2025-08-08 15:53:14 +0530

    Time: 0 ms (100%), Space: 16.6 MB (88.63%) - LeetHub

---
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        queue<pair<int, int>>q;
+        int n = grid.size();
+        int m = grid[0].size();
+        int freshOr = 0;
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(grid[i][j] == 2) q.push({i, j});
+                if(grid[i][j] == 1) freshOr++;
+            }
+        }
+        int cnt = -1;
+        if(freshOr == 0) return 0;
+        q.push({-1, -1});
+        while(!q.empty()){
+            auto a = q.front();
+            q.pop();
+            int i = a.first;
+            int j = a.second;
+            if (i == -1 && j == -1) {
+            cnt++;
+            if (!q.empty()) { // ✅ Only push if there’s another level
+                q.push({-1, -1});
+            }
+            continue; // Skip neighbor checks
+        }
+        else{
+            if((i-1>=0 ) && grid[i-1][j] == 1){
+              q.push({i-1, j});
+              grid[i-1][j] = 2;
+              freshOr -= 1;
+          }
+      
+          if((i+1<grid.size() ) && grid[i+1][j] == 1){
+              q.push({i+1, j});
+              grid[i+1][j] = 2;
+              freshOr -= 1;
+          }
+          
+          if((j-1>=0 ) && grid[i][j-1] == 1){
+              q.push({i, j-1});
+              grid[i][j-1] = 2;
+              freshOr -= 1;
+          }
+          
+          if((j+1<grid[0].size() ) && grid[i][j+1] == 1){
+              q.push({i, j+1});
+              grid[i][j+1] = 2;
+              freshOr -= 1;
+          }
+            }
+        }
+        return (freshOr > 0) ? -1 : cnt;;
+    }
+};
