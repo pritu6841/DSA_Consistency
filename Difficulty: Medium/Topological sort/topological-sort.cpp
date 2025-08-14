21c627050433ff8d7b13631992dd220de13f398d
pritu6841 <pritu6841@gmail.com>
2025-08-14 14:49:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Topological sort/topological-sort.cpp
@@ -0,0 +1,32 @@
+class Solution {
+  public:
+    vector<int> topoSort(int V, vector<vector<int>>& edges) {
+        // code here
+        vector<vector<int>>adj(V);
+        
+        for(int i = 0; i<edges.size(); i++){
+            adj[edges[i][0]].push_back(edges[i][1]);
+        }
+        
+        vector<int>indegree(V, 0);
+        for(int i = 0; i<edges.size(); i++){
+            indegree[edges[i][1]]++;
+        }
+        queue<int>q;
+        for(int i =0; i<V; i++) {
+            if(indegree[i] == 0) q.push(i);
+        }
+        vector<int>topo;
+        
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            for(auto & i: adj[node]){
+                indegree[i]--;
+                if(indegree[i] == 0)q.push(i);
+            }
+        }
+    return topo;
+    }
+};
