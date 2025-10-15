3c99162857c80f3f380effab1dd9dd5893b03c3c
pritu6841 <pritu6841@gmail.com>
2025-10-15 10:06:24 +0530

    Time: 0 ms (100%), Space: 15.8 MB (32.78%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,30 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
-        (next_permutation(nums.begin(), nums.end()));
+    void nextPermutation(vector<int>& arr) {
+        int n = arr.size();
+        int idx = -1;
+
+        //finding break point
+        for(int i = n-2; i>=0; i--){
+            if(arr[i] < arr[i+1]) {
+                idx = i;
+                break;
+            }
+        }
+
+        //base case
+        if(idx == -1) reverse(arr.begin(), arr.end());
+        else{
+        //find next greater element in the remaining array
+        for(int i = n-1; i>=0; i--) {
+            if(arr[i] > arr[idx]){
+                swap(arr[i], arr[idx]);
+                break;
+            }
+        }
+
+        //sort the remaining array
+         reverse(arr.begin()+idx+1, arr.end());
+         }
     }
 };
